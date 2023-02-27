48f460649d8ce15369ad3bdd4e4e352ee3d6a55a
ndowkunda <ndowkunda@users.noreply.github.com>
2023-02-27 20:30:45 +0000

    add error handling for applicant age

--- __tests__/utils/ageValidator.test.js
+++ __tests__/utils/ageValidator.test.js
@@ -1,7 +1,50 @@
 const AgeValidator = require("../../src/utils/AgeValidator");
 
-describe("validate age", () => {
-  test("should return whether age is a valid age in loan application", () => {
+
+describe("Validate applicant's age", () => {
+  test("should throw error when date of birth is invalid due to non-existent day", () => {
+    const mockApplicantReq = {
+      dateOfBirth: "2000-02-30",
+      annualIncome: "50000",
+      loanAmount: "10000",
+      residentialMonthlyExpenditure: "900",
+    };
+
+    const ageValidator = new AgeValidator(25);
+
+    expect(() => {
+      ageValidator.calculateAgeInYears(mockApplicantReq.dateOfBirth);
+    }).toThrowError("Invalid date of birth");
+  });
+  test("should throw error when date of birth is invalid due to non-existent month", () => {
+    const mockApplicantReq = {
+      dateOfBirth: "2000-13-28",
+      annualIncome: "50000",
+      loanAmount: "10000",
+      residentialMonthlyExpenditure: "900",
+    };
+
+    const ageValidator = new AgeValidator(25);
+
+    expect(() => {
+      ageValidator.calculateAgeInYears(mockApplicantReq.dateOfBirth);
+    }).toThrowError("Invalid date of birth");
+  });
+  test("should throw error when date of birth is invalid due to invalid date format", () => {
+    const mockApplicantReq = {
+      dateOfBirth: "22-02-2000",
+      annualIncome: "50000",
+      loanAmount: "10000",
+      residentialMonthlyExpenditure: "900",
+    };
+
+    const ageValidator = new AgeValidator(25);
+
+    expect(() => {
+      ageValidator.calculateAgeInYears(mockApplicantReq.dateOfBirth);
+    }).toThrowError("Invalid date of birth");
+  });
+  test("should return applicant's age is not valid", () => {
     const mockApplicantReq = {
       dateOfBirth: "2005-02-20",
       annualIncome: "50000",
@@ -12,7 +55,7 @@ describe("validate age", () => {
     const isValidAge = ageValidator.validate(mockApplicantReq);
     expect(isValidAge).toBe(false);
   });
-  test("should return age of applicant in years", () => {
+  test("should return applicant's age in years", () => {
     const mockApplicantReq = {
       dateOfBirth: "2005-02-20",
       annualIncome: "50000",
--- src/utils/AgeValidator.js
+++ src/utils/AgeValidator.js
@@ -11,6 +11,9 @@ class AgeValidator {
   calculateAgeInYears(dateOfBirth) {
     const birthDate = new Date(dateOfBirth);
     const currentDate = new Date();
+     if (dateOfBirth !== birthDate.toLocaleDateString("en-CA")) {
+       throw new Error("Invalid date of birth");
+     }
     const diffInMillseconds = currentDate - birthDate;
     const ageInYears = Math.floor(
       diffInMillseconds / (1000 * 60 * 60 * 24 * 365.25)
