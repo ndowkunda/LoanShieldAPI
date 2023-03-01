531bc391adce060dfc348cde33dc9631c056f2a0
ndowkunda <ndowkunda@users.noreply.github.com>
2023-03-01 02:14:04 +0000

    add error handling for `LoanValidator` class

--- src/utils/LoanValidator.js
+++ src/utils/LoanValidator.js
@@ -1,18 +1,34 @@
 class LoanValidator {
   constructor(rules) {
     this.rules = rules;
+    this.errors = [];
+    this.validationErrors = [
+      "Invalid date of birth",
+      "Annual income must be a numeric value",
+      "Loan amount must be a numeric value",
+      "Residential monthly expenditure must be a numeric value",
+    ];
+  }
+
+  containsErrors() {
+    return this.errors.some((error) => this.validationErrors.includes(error));
   }
 
   validate(loanApplication) {
-    const errorMessages = this.rules.map((rule) => {
-      return rule.validate(loanApplication) ? null : rule.errorMessage;
+    const isValid = this.rules.every((rule) => {
+      try {
+        this.errors.push(...rule.errors);
+        return rule.validate(loanApplication);
+      } catch (error) {
+        this.errors.push(error.message);
+        return false;
+      }
     });
 
-    const errors = errorMessages.filter((message) => message);
-    if (errors.length > 0) {
-      throw new Error(`Loan cannot be submitted due to ${errors.join(", ")}`);
+    if (this.containsErrors()) {
+      throw new Error("Loan application invalid");
     }
-    return true;
+    return isValid;
   }
 }
 
