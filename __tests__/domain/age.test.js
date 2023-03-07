27d7e2fa03a3855fb02770a8966360d1ac4c1f8f
ndowkunda <ndowkunda@users.noreply.github.com>
2023-03-07 11:26:05 +0000

    refactor test cases - add mockCustomer object

--- __tests__/domain/age.test.js
+++ __tests__/domain/age.test.js
@@ -1,4 +1,5 @@
 const Age = require("../../src/domain/Age");
+const { mockCustomerReq } = require("../../__mocks__/mockCustomer.mock");
 let age;
 
 beforeEach(() => {
@@ -7,59 +8,36 @@ beforeEach(() => {
 
 describe("Validate customer's age", () => {
   test("should throw error when date of birth is invalid due to non-existent day", () => {
-    const mockCustomerReq = {
-      dateOfBirth: "2000-02-30",
-      annualIncome: "50000",
-      loanAmount: "10000",
-      residentialMonthlyExpenditure: "900",
-    };
+    mockCustomerReq.body.dateOfBirth = "2000-02-30";
 
     expect(() => {
       age.validate(mockCustomerReq);
     }).toThrowError("Invalid date of birth");
   });
   test("should throw error when date of birth is invalid due to non-existent month", () => {
-    const mockCustomerReq = {
-      dateOfBirth: "2000-13-28",
-      annualIncome: "50000",
-      loanAmount: "10000",
-      residentialMonthlyExpenditure: "900",
-    };
+    mockCustomerReq.body.dateOfBirth = "2000-13-28";
 
     expect(() => {
       age.validate(mockCustomerReq);
     }).toThrowError("Invalid date of birth");
   });
   test("should throw error when date of birth is invalid due to invalid date format", () => {
-    const mockCustomerReq = {
-      dateOfBirth: "22-02-2000",
-      annualIncome: "50000",
-      loanAmount: "10000",
-      residentialMonthlyExpenditure: "900",
-    };
+    mockCustomerReq.body.dateOfBirth = "22-02-2000";
 
     expect(() => {
       age.validate(mockCustomerReq);
     }).toThrowError("Invalid date of birth");
   });
   test("should return customer's age is not valid", () => {
-    const mockCustomerReq = {
-      dateOfBirth: "2005-02-20",
-      annualIncome: "50000",
-      loanAmount: "10000",
-      residentialMonthlyExpenditure: "900",
-    };
-    const isValidAge = age.validate(mockCustomerReq);
+    mockCustomerReq.body.dateOfBirth = "2005-02-20";
+
+    const isValidAge = age.validate(mockCustomerReq.body);
     expect(isValidAge).toBe(false);
   });
   test("should return customer's age is valid", () => {
-    const mockCustomerReq = {
-      dateOfBirth: "1997-02-20",
-      annualIncome: "50000",
-      loanAmount: "10000",
-      residentialMonthlyExpenditure: "900",
-    };
-    const isValidAge = age.validate(mockCustomerReq);
+    mockCustomerReq.body.dateOfBirth = "1996-02-20";
+
+    const isValidAge = age.validate(mockCustomerReq.body);
     expect(isValidAge).toBe(true);
   });
 });
